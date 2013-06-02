b850345c6a4de1b8b25e493664274e5a3f9a71ba
Flambino <daniel@stimulacrum.com>
2013-06-02 18:17:50 +0200

    Added bracket-style getters/setters

--- lib/week_sauce.rb
+++ lib/week_sauce.rb
@@ -1,5 +1,9 @@
+require 'date'
+
 class WeekSauce
   MAX_VALUE = 2**7 - 1
+  DAY_NAMES = %w(sunday monday tuesday wednesday thursday friday saturday).map(&:to_sym).freeze
+  DAY_BITS  = Hash[ DAY_NAMES.zip(Array.new(7) { |i| 2**i }) ].freeze
   
   def initialize(value = nil)
     @value = [[0, value.to_i].max, MAX_VALUE].min
@@ -25,7 +29,45 @@ class WeekSauce
     any? && !single?
   end
   
+  def [](wday)
+    get coerce_to_bit(wday)
+  end
+  
+  def []=(wday, bool)
+    set coerce_to_bit(wday), bool
+  end
+  
   def to_i
     @value
   end
+  
+  private
+  
+    def get(bit)
+      if DAY_BITS.values.include?(bit)
+        @value & bit > 0
+      end
+    end
+    
+    def set(bit, set)
+      if DAY_BITS.values.include?(bit)
+        if set
+          @value = @value | bit
+        else
+          @value = @value & (MAX_VALUE ^ bit)
+        end
+      end
+    end
+    
+    def coerce_to_bit(wday)
+      case wday
+      when Symbol
+        DAY_BITS[wday]
+      when Fixnum
+        (0..6).include?(wday) ? 2**wday : nil
+      when Date, Time
+        2**wday.wday
+      end
+    end
+  
 end
--- spec/week_sauce_spec.rb
+++ spec/week_sauce_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
+require 'active_support/time'
 
 describe WeekSauce do
   
   let(:days) do
-    # Sunday == 0 similar to Date#wday
+    # Sunday => 0 similar to Time#wday
     %w(sunday monday saturday wednesday thursday friday saturday).map(&:to_sym)
   end
   
-  # The maximum value of the bitmask (127)
-  let(:max) { 2**7 - 1 }
+  # The maximum value of the bitmask (2**7 - 1)
+  let(:max) { 127 }
   
   describe "initializer" do
     it "defaults to zero/blank" do
@@ -74,4 +75,124 @@ describe WeekSauce do
     end
   end
   
+  describe "bracket setter" do
+    let(:week) { WeekSauce.new }
+    it "works with integers" do
+      week[0] = true
+      week[0] = true # repeated on purpose to expose bitwise XOR errors
+      week.to_i.should == 1
+      week[6] = true
+      week.to_i.should == 65
+    end
+    
+    it "ignores out-of-bounds integers" do
+      week[-1] = true
+      week.to_i.should == 0
+      week[7] = true
+      week.to_i.should == 0
+    end
+    
+    it "works with day-name symbols" do
+      week[:sunday] = true
+      week.to_i.should == 1
+      week[:wednesday] = true
+      week.to_i.should == 9
+    end
+    
+    it "ignores non-sensical symbols" do
+      week[:foo] = true
+      week.to_i.should == 0
+    end
+    
+    it "works with Time and TimeWithZone objects" do
+      week[Time.now] = true
+      week.to_i.should == 2**Time.now.wday
+      
+      Time.zone = ActiveSupport::TimeZone["Copenhagen"]
+      time = Time.zone.now
+      week[time] = true
+      week.to_i.should == 2**time.wday
+    end
+    
+    it "works with Date and DateTime objects" do
+      date = Date.today
+      week[date] = true
+      week.to_i.should == 2**date.wday
+      
+      date = DateTime.now
+      week[date] = true
+      week.to_i.should == 2**date.wday
+    end
+    
+    it "ignores unhandled argument types" do
+      week["string"] = true
+      week.to_i.should == 0
+    end
+  end
+  
+  describe "bracket getter" do
+    let(:week) { WeekSauce.new(42) } # Monday, Wednesday, Friday
+    
+    it "works with integers" do
+      week[0].should be_false
+      week[1].should be_true
+      week[2].should be_false
+      week[3].should be_true
+      week[4].should be_false
+      week[5].should be_true
+      week[6].should be_false
+    end
+    
+    it "returns nil for out-of-bounds integers" do
+      week[-1].should be_nil
+      week[7].should be_nil
+    end
+    
+    it "works with day-name symbols" do
+      week[:sunday].should be_false
+      week[:monday].should be_true
+      week[:tuesday].should be_false
+      week[:wednesday].should be_true
+      week[:thursday].should be_false
+      week[:friday].should be_true
+      week[:saturday].should be_false
+    end
+    
+    it "returns nil for non-sensical symbols" do
+      week[:foo].should be_nil
+    end
+    
+    it "works with Time and TimeWithZone objects" do
+      time = Time.now
+      time = time + (3 - time.wday).days # set to Wednesday
+      week[time].should be_true
+      time = time + 1.day # set to Thursday
+      week[time].should be_false
+      
+      Time.zone = ActiveSupport::TimeZone["Copenhagen"]
+      time = Time.zone.now
+      time = time + (3 - time.wday).days
+      week[time].should be_true
+      time = time + 1.day # set to Thursday
+      week[time].should be_false
+    end
+    
+    it "works with Date and DateTime objects" do
+      date = Date.today
+      date = date + (3 - date.wday).days # set to Wednesday
+      week[date].should be_true
+      date = date + 1.day # set to Thursday
+      week[date].should be_false
+      
+      date = DateTime.now
+      date = date + (3 - date.wday).days # set to Wednesday
+      week[date].should be_true
+      date = date + 1.day # set to Thursday
+      week[date].should be_false
+    end
+    
+    it "returns nil for unhandled argument types" do
+      week["string"].should be_nil
+    end
+  end
 end
