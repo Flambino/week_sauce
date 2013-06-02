943aaa71415daa19590b22d427e732d0368a561a
Flambino <daniel@stimulacrum.com>
2013-06-02 16:13:44 +0200

    Added basic interrogation methods

--- lib/week_sauce.rb
+++ lib/week_sauce.rb
@@ -1,3 +1,31 @@
 class WeekSauce
+  MAX_VALUE = 2**7 - 1
   
+  def initialize(value = nil)
+    @value = [[0, value.to_i].max, MAX_VALUE].min
+  end
+  
+  def blank?
+    @value == 0
+  end
+  
+  def all?
+    @value == MAX_VALUE
+  end
+  
+  def any?
+    !blank?
+  end
+  
+  def single?
+    any? && @value & (@value - 1) == 0
+  end
+  
+  def many?
+    any? && !single?
+  end
+  
+  def to_i
+    @value
+  end
 end
--- spec/week_sauce_spec.rb
+++ spec/week_sauce_spec.rb
@@ -1,5 +1,77 @@
 require 'spec_helper'
 
 describe WeekSauce do
-  pending "tests"
+  
+  let(:days) do
+    # Sunday == 0 similar to Date#wday
+    %w(sunday monday saturday wednesday thursday friday saturday).map(&:to_sym)
+  end
+  
+  # The maximum value of the bitmask (127)
+  let(:max) { 2**7 - 1 }
+  
+  describe "initializer" do
+    it "defaults to zero/blank" do
+      WeekSauce.new.to_i.should == 0
+      WeekSauce.new(nil).to_i.should == 0
+    end
+    
+    it "accepts valid numbers" do
+      WeekSauce.new(0).to_i.should   == 0
+      WeekSauce.new(64).to_i.should  == 64
+      WeekSauce.new(max).to_i.should == max
+    end
+    
+    it "clamps input" do
+      WeekSauce.new(-100).to_i.should == 0
+      WeekSauce.new(2**9).to_i.should == max
+    end
+  end
+  
+  describe "interrogation" do
+    context "with no days set" do
+      let(:week) { WeekSauce.new }
+      it "returns correct values" do
+        week.blank?.should  be_true
+        week.any?.should    be_false
+        week.many?.should   be_false
+        week.single?.should be_false
+        week.all?.should    be_false
+      end
+    end
+    
+    context "with only one day set" do
+      let(:week) { WeekSauce.new(16) }
+      it "returns correct values" do
+        week.blank?.should  be_false
+        week.any?.should    be_true
+        week.many?.should   be_false
+        week.single?.should be_true
+        week.all?.should    be_false
+      end
+    end
+    
+    context "with more than one day set" do
+      let(:week) { WeekSauce.new(31) }
+      it "returns correct values" do
+        week.blank?.should  be_false
+        week.any?.should    be_true
+        week.many?.should   be_true
+        week.single?.should be_false
+        week.all?.should    be_false
+      end
+    end
+    
+    context "with all days set" do
+      let(:week) { WeekSauce.new(max) }
+      it "returns correct values" do
+        week.blank?.should  be_false
+        week.any?.should    be_true
+        week.many?.should   be_true
+        week.single?.should be_false
+        week.all?.should    be_true
+      end
+    end
+  end
+  
 end
